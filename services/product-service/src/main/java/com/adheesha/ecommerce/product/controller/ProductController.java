5d512d4b30e37c7c1c0f7acdf6befec9f3624b64
Adheesha Chamod <adheeshachamoddesilva@gmail.com>
2025-03-03 19:49:52 +0530

    Add files

---
+++ services/product-service/src/main/java/com/adheesha/ecommerce/product/controller/ProductController.java
@@ -0,0 +1,16 @@
+package com.adheesha.ecommerce.product.controller;
+
+import com.adheesha.ecommerce.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+
+}
